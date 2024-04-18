3926daded938413281323dac0fcf7dbdda8f7467
Remel Crowe <remcrw@Remels-MacBook-Pro.local>
2024-04-18 17:31:55 +0100

    Added tests for user stories 1-4

--- __tests__/testing-framework.js
+++
@@ -1,1 +0,0 @@
-export const assertEqual = (actual, expected) => actual === expected;
--- src/basket.js
+++ src/basket.js
@@ -1,8 +1,27 @@
  const basket = {
-    items: [],
-    addItem(item) {
-        this.items.push(item)
-    },
+     items: [],
+     capacity: 5,
+     checkType(item) {
+         if (item === null) {
+             return false
+         } 
+        return true
+     },
+     addItem(item) {
+         let check = this.checkType(item)
+         check && !this.isFull() ? this.items.push(item) : ''
+     },
+     removeItem(item) {
+        this.items = this.items.filter(items => items !== item)
+     },
+     isFull() {
+         return this.items.length === 5
+     },
+     setCapacity(size) {
+         size >= 1 ? this.capacity = size : ''
+     }
+
+     
  }
 
 
